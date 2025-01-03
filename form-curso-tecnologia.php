b98676cb26ce841439e0dc4bf36b1c08a063a4fb
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-03 11:58:16 -0300

    Create form-curso-tecnologia.php

---
+++ form-curso-tecnologia.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CursosTec</title>
+</head>
+<body>
+    <form action="enviar-form-curso-tecnologia.php" method="get">
+        digite o nome do curso de tecnologia <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição do curso de tecnologia <br>
+        <input type="text" name="descricao"> <br>
+        digite a carga do curso de tecnologia <br>
+        <input type="number" name="carga"> <br>
+        digite total de aulas do curso de tecnologia <br>
+        <input type="number" name="totaulas"> <br>
+        digite a imagem do curso de tecnologia <br>
+        <input type="text" name="imagem"> <br>
+        digite a playlist do curso de tecnologia <br>
+        <input type="text" name="playlist"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
